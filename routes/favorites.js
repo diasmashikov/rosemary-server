1809ac09c84aff7e9f13e41bf38c8929bae4d51e
Dias <diasmashikovnasa@gmail.com>
2021-09-14 09:11:48 -0700

    Fixed bug within favorites

--- routes/favorites.js
+++ routes/favorites.js
@@ -17,7 +17,7 @@ function getAllFavoritesByUser() {
   router.get(`/:userId`, async (req, res) => {
     const favoritesList = await _getAllByFavoritesByUserFromMongoDB(
       req.params.userId
-    );
+    ).populate("products");
 
     ResponseController.sendResponse(
       res,
